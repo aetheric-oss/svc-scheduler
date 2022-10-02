37c8c400b2c33e304f5314d6bd5f15c7994ea790
Arrow automation <automation@arrowair.com>
2022-10-02 10:30:50 +0000

    Provisioned by Terraform

---
+++ .make/cspell.mk
@@ -0,0 +1,20 @@
+## DO NOT EDIT!
+# This file was provisioned by Terraform
+# File origin: https://github.com/Arrow-air/tf-github/tree/main/src/templates/all/.make/cspell.mk
+
+.help-cspell:
+	@echo ""
+	@echo "$(SMUL)$(BOLD)$(GREEN)CSpell$(SGR0)"
+	@echo "  $(BOLD)cspell-test$(SGR0)      -- Run 'cspell --words-only --unique "**/**" -c .cspell.config.yaml'"
+	@echo "                      to validate files are not containing any spelling errors."
+	@echo "  $(BOLD)cspell-add-words$(SGR0) -- Run 'cspell --words-only --unique "**/**" -c .cspell.config.yaml | "
+	@echo "                      sort --ignore-case >> .cspell.project-words.txt'"
+	@echo "                      to add remaining words to the project's cspell ignore list"
+
+cspell-test: docker-pull
+	@echo "$(CYAN)Checking for spelling errors...$(SGR0)"
+	@$(call docker_run,cspell --words-only --unique "**/**" -c .cspell.config.yaml)
+
+cspell-add-words: docker-pull
+	@echo "$(CYAN)Adding words to the project's cspell word list...$(SGR0)"
+	@$(call docker_run,sh -c 'cspell --words-only --unique "**/**" -c .cspell.config.yaml 2> /dev/null | sort -f >> .cspell.project-words.txt')
