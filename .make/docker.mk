0ad2b74e92e9bdc7b9476c851d62287c872e0055
Arrow automation <automation@arrowair.com>
2022-10-04 16:59:18 +0000

    Provisioned by Terraform

--- .make/docker.mk
+++ .make/docker.mk
@@ -3,11 +3,11 @@
 # File origin: https://github.com/Arrow-air/tf-github/tree/main/src/templates/all/.make/docker.mk
 
 DOCKER_BUILD_PATH ?= .
-IMAGE_NAME        ?= ${name}
+IMAGE_NAME        ?= $(PACKAGE_NAME)
 
 .help-docker:
 	@echo ""
-	@echo "$(SMUL)$(BOLD)$(GREEN)Docker targets$(NC)$(SGR0)"
+	@echo "$(SMUL)$(BOLD)$(GREEN)Docker targets$(SGR0)"
 	@echo "  $(BOLD)docker-build$(SGR0)-- Run docker build to create new image"
 	@echo "  $(BOLD)docker-run$(SGR0)  -- Run docker container as a daemon, binding port $(HOST_PORT):$(DOCKER_PORT)"
 	@echo "  $(BOLD)docker-stop$(SGR0) -- Run 'docker kill $${DOCKER_NAME}-run' to stop our docker after running"
@@ -18,7 +18,7 @@ docker-build:
 docker-run: docker-stop
 	# Run docker container as a daemon and map a port
 	@docker run --rm -d -p $(HOST_PORT):$(DOCKER_PORT) --name=$(DOCKER_NAME)-run $(IMAGE_NAME):latest
-	@echo "$(YELLOW)Docker running and listening to http://localhost:$(HOST_PORT)$(NC)"
+	@echo "$(YELLOW)Docker running and listening to http://localhost:$(HOST_PORT)$(SGR0)"
 
 docker-stop:
 	@docker kill ${DOCKER_NAME}-run || true
