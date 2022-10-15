91bcd1331951a3758ac373f54c9da55edc4ca338
Arrow automation <automation@arrowair.com>
2022-10-15 08:23:18 +0000

    Provisioned by Terraform

--- .make/editorconfig.mk
+++ .make/editorconfig.mk
@@ -6,6 +6,7 @@
 	@echo ""
 	@echo "$(SMUL)$(BOLD)$(GREEN)Editorconfig$(SGR0)"
 	@echo "  $(BOLD)editorconfig-test$(SGR0) -- Run editorconfig test to validate all file formats are valid"
+	@echo "  $(BOLD)editorconfig-tidy$(SGR0) -- Run editorconfig tidy to fix all file formats if needed"
 
 editorconfig-test:
 	@echo "$(CYAN)Checking if the codebase is compliant with the .editorconfig file...$(SGR0)"
@@ -16,3 +17,7 @@ editorconfig-test:
 		-w "/usr/src/app" \
 		-v "$(PWD):/usr/src/app" \
 		-t mstruebing/editorconfig-checker
+
+editorconfig-tidy: docker-pull
+	@echo "$(CYAN)Running editorconfig formatting fixes...$(SGR0)"
+	@$(call docker_run,sh -c 'eclint fix .')
